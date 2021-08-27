e67a42d6fe3dd6b7a41cad9b33b045debefb7796
atulnk <atulnk1@gmail.com>
2021-08-27 17:27:50 +0800

    basic show page

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -16,4 +16,19 @@ controller.get("/", async (req, res) => {
     }
 });
 
+controller.get("/:id", async (req, res) => {
+    try{
+        const property = await propertyModel.findById(req.params.id)
+        // console.log(property)
+        res.render("property/show.ejs", {
+            property
+        });
+    } catch (e) {
+        res.status(400).send({
+            name: e.name,
+            message: e.message
+        })
+    }
+})
+
 module.exports = controller;
