6a15d91da2bc1d331b353793d497e60864c8b128
atulnk <atulnk1@gmail.com>
2021-08-28 17:45:28 +0800

    edit route added

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const propertyModel = require("../models/property");
 const controller = express.Router();
 
+// INDEX PAGE
 controller.get("/", async (req, res) => {
     try {
         const properties = await propertyModel.find({});
@@ -15,7 +16,7 @@ controller.get("/", async (req, res) => {
         })
     }
 });
-
+// GET ADD NEW PROPERTY PAGE
 controller.get("/new", async (req, res) => {
     try {
         res.render("property/new.ejs")
@@ -26,7 +27,7 @@ controller.get("/new", async (req, res) => {
         })
     }
 });
-
+// GET ONE PROPERTY ROUTE
 controller.get("/:id", async (req, res) => {
     try{
         const property = await propertyModel.findById(req.params.id)
@@ -41,13 +42,14 @@ controller.get("/:id", async (req, res) => {
         })
     }
 });
-
+// ADD NEW PROPERTY ROUTE
 controller.post("/", async (req, res) => {
-    const imageList = req.body.image.split(',')
-    const newHistoricalValue = req.body.historicalValue.split(',');
-    const newHistoricalRentalAmount = req.body.historicalRentalAmount.split(',');
+    
 
     try {
+        const imageList = req.body.image.split(',')
+        const newHistoricalValue = req.body.historicalValue.split(',');
+        const newHistoricalRentalAmount = req.body.historicalRentalAmount.split(',');
         inputs = {
             name: req.body.name,
             unit: req.body.unit,
@@ -80,6 +82,61 @@ controller.post("/", async (req, res) => {
     } 
 });
 
+// UPDATE ROUTES
+controller.get('/:id/edit', async (req, res) => {
+    try {
+        const selectedProperty = await propertyModel.findById(req.params.id)
+        res.render('property/edit.ejs', {
+            property: selectedProperty
+        })
+    } catch (e) {
+        res.status(400).send({
+            name: e.name,
+            message: e.message
+        })
+    }
+});
+
+controller.put('/:id', async (req, res) => {
+    try {
+        const imageList = req.body.image.split(',')
+        const newHistoricalValue = req.body.historicalValue.split(',');
+        const newHistoricalRentalAmount = req.body.historicalRentalAmount.split(',');
+        inputs = {
+            name: req.body.name,
+            unit: req.body.unit,
+            street: req.body.street,
+            postalCode: req.body.postalCode,
+            district: req.body.district,
+            image: imageList,
+            googleMapLink: req.body.googleMapLink,
+            propertyType: req.body.propertyType,
+            purchaseValue: req.body.purchaseValue, // Amount user bought it for
+            currentValue: req.body.currentValue, // Current price for property
+            historicalValue: newHistoricalValue,
+            rentalAmount: req.body.rentalAmount,
+            historicalRentalAmount: newHistoricalRentalAmount,
+            status: req.body.status,
+            installmentAmount: req.body.installmentAmount,
+            loanLeft: req.body.loanLeft
+        }
+
+        console.log(inputs)
+        await propertyModel.updateOne({
+            _id: req.params.id,
+        }, inputs);
+
+        res.redirect(`/property/${req.params.id}`)
+
+    } catch (e) {
+        res.status(400).send({
+            name: e.name,
+            message: e.message
+        })
+    }
+});
+
+// DELETE ROUTE
 controller.delete('/:id', async (req, res) => {
     try {
         await propertyModel.deleteOne({
