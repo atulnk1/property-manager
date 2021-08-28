3d3804f807033933d7aa11b408c0ff3b91d79f49
atulnk <atulnk1@gmail.com>
2021-08-28 15:10:16 +0800

    added basic new.ejs and path

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -16,6 +16,17 @@ controller.get("/", async (req, res) => {
     }
 });
 
+controller.get("/new", async (req, res) => {
+    try {
+        res.render("property/new.ejs")
+    } catch (e) {
+        res.status(400).send({
+            name: e.name,
+            message: e.message
+        })
+    }
+});
+
 controller.get("/:id", async (req, res) => {
     try{
         const property = await propertyModel.findById(req.params.id)
@@ -29,6 +40,41 @@ controller.get("/:id", async (req, res) => {
             message: e.message
         })
     }
+});
+
+controller.post("/", async (req, res) => {
+    try {
+        inputs = {
+            name: req.body.name,
+            unit: req.body.unit,
+            street: req.body.street,
+            postalCode: req.body.postalCode,
+            district: req.body.district,
+            image: req.body.image,
+            googleMapLink: req.body.googleMapLink,
+            propertyType: req.body.propertyType,
+            purchaseValue: req.body.purchaseValue, // Amount user bought it for
+            currentValue: req.body.currentValue, // Current price for property
+            historicalValue: req.body.historicalValue,
+            rentalAmount: req.body.rentalAmount,
+            historicalRentalAmount: req.body.historicalRentalAmount,
+            status: req.body.status,
+            installmentAmount: req.body.installmentAmount,
+            loanLeft: req.body.loanLeft
+        }
+
+        console.log(inputs)
+        await propertyModel.create(inputs)
+
+        res.redirect("/property")
+
+    } catch (e) {
+        res.status(400).send({
+            name: e.name,
+            message: e.message
+        })
+    } 
 })
+
 
 module.exports = controller;
